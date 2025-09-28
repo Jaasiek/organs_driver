1f7ebbc26c5f91a316bb132011aa9d37d70c9fee
Jasiek <u119_jangaw_waw@technischools.com>
2025-09-28 15:06:04 +0200

    Update Settings component to redirect after 4 seconds and modify access message

--- src/components/settings/settings.jsx
+++ src/components/settings/settings.jsx
@@ -1,11 +1,20 @@
 import "./settings.scss";
 import ControlPanelHeader from "../controlPanelHeader/controlPanelHeader";
+import { route } from "preact-router";
+import { useEffect } from "react";
 
 export default function Settings() {
+  useEffect(() => {
+    setTimeout(() => {
+      route("/controlPanel");
+    }, 4000);
+  }, []);
+
   return (
     <div className="settings">
       <ControlPanelHeader />
-      <p>Settings</p>
+      <h2>Nie masz dostępu do tej części</h2>
+      <h4>Za chwilę zostaniesz automatycznie przekierowany na stronę główną</h4>
     </div>
   );
 }
--- src/socket.jsx
+++ src/socket.jsx
@@ -2,7 +2,8 @@ import { io } from "socket.io-client";
 
 // const socket = io("http://192.168.6.20:2137"); // or your server IP
 // const socket = io("http://192.168.6.24:2137"); // or your server IP
-const socket = io("http://192.168.0.41:2137");
-// const socket = io("http://192.168.0.91:2137");
+// const socket = io("http://192.168.0.41:2137");
+// const socket = io("http://192.168.0.53:2137");
+const socket = io("http://192.168.0.3:2137");
 // const socket = io("http://192.168.6.48:2137");
 export default socket;
